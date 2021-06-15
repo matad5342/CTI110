f88d2bcb1fa645a3bc42cba8641f54addeb1c10a
matad5342 <85847840+matad5342@users.noreply.github.com>
2021-06-14 23:25:30 -0400

    5th submission

---
+++ P1HW2_BasicMath_Mata Danny.py
@@ -0,0 +1,21 @@
+ # Math
+    # 14 JUNE 21
+    # CTI-110 P1HW2 - Basic Math
+    # Danny Mata
+x = int(input("Enter an integer"))
+y = int(input("Enter another integer"))
+sum = (x+y)
+sentence = 'The sum of {} and {} is {}.'.format(x, y, sum)
+print(sentence)
+product = (x*y)
+sentence = 'The product of {} and {} is {}.'.format(x, y, product)
+print(sentence)
+x = int(input('please enter first integer'))
+y = int(input('please enter second integer'))
+int(input('first number entered'))
+int(input('second number entered'))
+sum = (x+y)
+sentence = 'The sum of {} and {} is {}.'.format(x, y, sum)
+print(sentence)
+
+
