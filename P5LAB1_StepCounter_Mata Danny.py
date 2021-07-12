bb459208b2b7c637be55400b50f0ce42aa01cd59
matad5342 <85847840+matad5342@users.noreply.github.com>
2021-07-12 19:22:32 -0400

    15th

---
+++ P5LAB1_StepCounter_Mata Danny.py
@@ -0,0 +1,8 @@
+def main():
+    steps=int(input('enter your number of steps:'));
+    miles=steps/2000;
+    print('miles walked')
+    if __name__ == '__main__':
+        print('{:.2f}'. format(miles));
+
+main()
